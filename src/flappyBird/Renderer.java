aa2b064fdc99024e85536e803852e7693ad0246e
i-git-it <34692181+i-git-it@users.noreply.github.com>
2017-12-19 17:38:06 -0600

    spelling fix

--- src/flappyBird/Renderer.java
+++ src/flappyBird/Renderer.java
@@ -9,6 +9,6 @@ public class Renderer extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	// this coment is here just to see if i can pull it to eclipse
+	// this comment is here just to see if i can pull it to eclipse
 
 }
