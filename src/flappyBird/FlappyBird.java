2b08708d2c61678e9a46cdf42f4f3a1b50bba14f
i-git-it <34692181+i-git-it@users.noreply.github.com>
2017-12-20 20:49:49 -0600

    Colored background, bird, ground and grass

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,10 +1,15 @@
 package flappyBird;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class FlappyBird {
+public class FlappyBird implements ActionListener {
 
 	public static FlappyBird flappyBird;
 	
@@ -12,20 +17,47 @@ public class FlappyBird {
 	
 	public Renderer renderer;
 	
+	public Rectangle bird;
+	
 	public FlappyBird(){
 		
+		
 		JFrame jframe = new JFrame(); 
+		Timer timer = new Timer(20,this);
+		
 		renderer = new Renderer();
 		
 		jframe.add(renderer);
+		jframe.setTitle("Flappy Bird");
 		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH,HEIGHT);
 		jframe.setVisible(true);
+		
+		bird = new Rectangle(WIDTH/2-10,HEIGHT/2-10,20,20);
+
+		
+		
+		timer.start();
 	}//end Constructor
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		renderer.repaint();
+	}
+	
 	public void repaint(Graphics g) {
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
+		g.setColor(Color.orange);
+		g.fillRect(0, HEIGHT-120, WIDTH, 120);
+		
+		g.setColor(Color.green);
+		g.fillRect(0, HEIGHT-120, WIDTH, 20);
+		
+		g.setColor(Color.red);
+		g.fillRect(bird.x, bird.y, bird.width, bird.height);
 	}
 	
 	public static void main(String[]args){
