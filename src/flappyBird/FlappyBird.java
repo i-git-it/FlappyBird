2842c78a64df7e3202de80aa444d7e4308f97f25
i-git-it <34692181+i-git-it@users.noreply.github.com>
2017-12-21 20:08:10 -0600

    Added moving Columns

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
@@ -19,6 +21,12 @@ public class FlappyBird implements ActionListener {
 	
 	public Rectangle bird;
 	
+	public int ticks, yMotion;
+	
+	public ArrayList<Rectangle> columns;
+	
+	public Random rand;
+	
 	public FlappyBird(){
 		
 		
@@ -26,6 +34,7 @@ public class FlappyBird implements ActionListener {
 		Timer timer = new Timer(20,this);
 		
 		renderer = new Renderer();
+		rand = new Random();
 		
 		jframe.add(renderer);
 		jframe.setTitle("Flappy Bird");
@@ -35,17 +44,62 @@ public class FlappyBird implements ActionListener {
 		jframe.setVisible(true);
 		
 		bird = new Rectangle(WIDTH/2-10,HEIGHT/2-10,20,20);
-
+		columns = new ArrayList<Rectangle>();
 		
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
+
 		
 		timer.start();
 	}//end Constructor
 	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		renderer.repaint();
+	public void addColumn(boolean start){
+		
+		int space =300;
+		int width = 100;
+		int height = 50 + rand.nextInt(300);
+		if (start){
+			columns.add(new Rectangle(WIDTH + width + columns.size()*300, HEIGHT -height - 120,width,height));
+			columns.add(new Rectangle(WIDTH + width + (columns.size() -1)*300, 0, width, HEIGHT - height - space ));	
+		}
+		else{
+			columns.add(new Rectangle(columns.get(columns.size()-1).x +600, HEIGHT -height - 120,width,height));
+			columns.add(new Rectangle(columns.get(columns.size() -1).x, 0, width, HEIGHT - height - space ));		}
+		
+		
 	}
 	
+	public void paintColumn(Graphics g, Rectangle column){
+		
+		g.setColor(Color.green.darker());
+		g.fillRect(column.x, column.y, column.width, column.height);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		int speed = 10;
+		ticks++;
+		
+		for(int i = 0; i < columns.size();i++)
+		{
+			Rectangle column = columns.get(i);
+			column.x -= speed;
+		}
+		
+		if(ticks %2 == 0 && yMotion <15)
+		{
+			yMotion+=2;
+		}
+		renderer.repaint();
+		
+		bird.y += yMotion;
+
+	}
+	
+
 	public void repaint(Graphics g) {
 		g.setColor(Color.cyan);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
@@ -58,6 +112,11 @@ public class FlappyBird implements ActionListener {
 		
 		g.setColor(Color.red);
 		g.fillRect(bird.x, bird.y, bird.width, bird.height);
+		
+		for(Rectangle column : columns)
+		{
+			paintColumn(g,column);
+		}
 	}
 	
 	public static void main(String[]args){
